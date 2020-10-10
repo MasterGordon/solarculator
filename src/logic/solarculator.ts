4a7a7b39114f8818e773d7b2ed73576707c9d27e
MasterGordon <gologo.x@gmail.com>
2020-10-10 10:36:00 +0200

    Initial commit

---
+++ src/logic/solarculator.ts
@@ -0,0 +1,12 @@
+import { observable } from "mobx";
+
+export default class SolarCulator {
+  @observable public powerUsage: number = 0;
+  @observable public roofArea: number = 0;
+  @observable public rootOrientation: number = 0;
+  @observable public roofAngle: number = 0;
+  @observable public longditude: number = 0;
+  @observable public latitude: number = 0;
+  @observable public consumtionTime: "tagsüber" | "unknown" = "unknown";
+  @observable public currentPowerPrice: number = 0;
+}
